f78d496c1d492352261f91bd45dbd7961eda5de9
Ravi Shankar Pandey <ravi.imscs@gmail.com>
2022-07-19 13:22:18 +0530

    "adds timestamp microservice functionality"

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 var express = require('express');
+require('dotenv').config();
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
@@ -18,14 +19,33 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function(req, res){
+  const date = new Date();
+  res.json({
+    unix:date.getTime(),
+    utc: date.toUTCString()
+  })
 });
-
-
-
+// your first API endpoint... 
+app.get("/api/:date", function (req, res) {
+  const date = req.params.date;
+  console.log(date);
+  let formattedDate = new Date(date);
+  console.log(formattedDate);
+  if(formattedDate.toString() === 'Invalid Date'){
+    formattedDate = new Date(parseInt(date));
+    console.log(formattedDate);
+  }
+  if(formattedDate.toString() === 'Invalid Date'){
+    return res.json({ error : "Invalid Date" });
+  }
+  else{
+    res.json({
+      unix:formattedDate.getTime(),
+      utc: formattedDate.toUTCString()
+    })
+  }
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
---
+++ timestamp.js
@@ -0,0 +1,14 @@
+// let express = require('express');
+// let app = express();
+// let bodyParser = require('body-parser');
+
+// // app.route('/api/:date').get(function(req,res,next){
+
+// // });
+
+// app.get('api/1451001600000', function(req, res){
+//     var jsonMessage = { unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" };
+//     res.send(jsonMessage);
+// });
+
+// module.exports = app;
